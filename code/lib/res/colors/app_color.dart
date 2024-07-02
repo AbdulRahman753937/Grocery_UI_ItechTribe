d9f6b574560d20977ae31517a141e4bb7e130ade
AbdulRahman753937 <abdulrahman937753@gmail.com>
2024-07-02 17:17:16 +0500

    Project pasted

---
+++ code/lib/res/colors/app_color.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+class AppColor {
+  static const Color blackColor = Color(0xff000000) ;
+  static const Color lowblack = Color(0xffC4C4C4);
+  static const Color whiteColor = Color(0xffffffff) ;
+  static const Color primarycolor = Color(0xffE2B378);
+  static const Color greencolor = Color(0xff00CD52);
+  static const Color rosebudcolor = Color(0xffFF9D2B);
+  static const Color greycolor = Color(0xffC4C4C4);
+  static const Color redcolor = Color(0xffF03333);
+}
---
+++ code/lib/view/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:groceries/res/colors/app_color.dart';
+
+class HomeScren extends StatelessWidget {
+  const HomeScren({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.redcolor,
+    );
+  }
+}
